b1d4872a509cd6e5b5e468b2c2055da801801080
AzSiAz <slider.perso@live.fr>
2019-03-05 09:19:47 +0100

    handle error

--- handler/server.go
+++ handler/server.go
@@ -11,11 +11,11 @@ import (
 
 type AddServerPost struct {
 	//AddedBy int `json:"added_by" form:"added_by"`
-	Name string `json:"name" form:"name"`
-	URL string `json:"url" form:"url"`
-	Port uint16 `json:"port" form:"port"`
+	Name    string        `json:"name" form:"name"`
+	URL     string        `json:"url" form:"url"`
+	Port    uint16        `json:"port" form:"port"`
 	Timeout time.Duration `json:"timeout" form:"timeout"`
-	Every time.Duration `json:"every" form:"every"`
+	Every   time.Duration `json:"every" form:"every"`
 }
 
 func (h *Handler) ListServer(c echo.Context) error {
@@ -32,23 +32,23 @@ func (h *Handler) AddServer(c echo.Context) error {
 	serverToAdd := new(AddServerPost)
 	if err := c.Bind(serverToAdd); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{
-			"Error": err.Error(),
+			"Error":   err.Error(),
 			"message": "Error getting correct struct",
 		})
 	}
 
 	mineProto := minecraftProtocol.MinecraftServer{
-		Name: serverToAdd.Name,
-		Every: serverToAdd.Every,
+		Name:    serverToAdd.Name,
+		Every:   serverToAdd.Every,
 		Timeout: serverToAdd.Timeout,
 		Address: serverToAdd.URL,
-		Port: serverToAdd.Port,
+		Port:    serverToAdd.Port,
 	}
 
 	srv, err := h.Store.AddServer(&mineProto, user.ID)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{
-			"Error": err.Error(),
+			"Error":   err.Error(),
 			"message": "Error saving server",
 		})
 	}
@@ -64,7 +64,7 @@ func (h *Handler) UpdateServer(c echo.Context) error {
 
 func (h *Handler) DeleteServer(c echo.Context) error {
 	idParam := c.Param("id")
-	if id == "" {
+	if idParam == "" {
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
 			"error": "Could not parse server ID",
 		})
@@ -76,7 +76,13 @@ func (h *Handler) DeleteServer(c echo.Context) error {
 		})
 	}
 
-	h.Store.DeleteServerByID(id)
+	err = h.Store.DeleteServerByID(id)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"error":   "error deleting server, try again",
+			"message": err.Error(),
+		})
+	}
 
 	return c.HTML(http.StatusOK, c.Param("id"))
 }
